37f885af49681fbabe86f2e96d84601383b60465
耿小泰 <clawpo@foxmail.com>
2017-03-22 21:57:18 +0800

    add butterknife

--- app/src/main/java/cn/ucai/retrofitdemo/MainActivity.java
+++ app/src/main/java/cn/ucai/retrofitdemo/MainActivity.java
@@ -1,13 +1,26 @@
 package cn.ucai.retrofitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_text)
+    TextView mTvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_get_user)
+    public void onClick() {
     }
 }
